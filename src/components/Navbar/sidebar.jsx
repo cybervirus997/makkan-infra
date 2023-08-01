fc68a6e0150439cb015370b964d9b8092c5d6a2d
Abhshek-Dewangan <callmeabhi1997@gmail.com>
2023-08-01 19:37:08 +0530

    Update routews

--- src/components/Navbar/sidebar.jsx
+++ src/components/Navbar/sidebar.jsx
@@ -8,28 +8,28 @@ export const Sidebar = ({ isSidebarOpen, handleSidebarToggle }) => {
         <li className='nav-item'>
           <a
             className='nav-link'
-            href='#'>
+            href='#/'>
             Home
           </a>
         </li>
         <li className='nav-item'>
           <a
             className='nav-link'
-            href='#'>
+            href='#/about'>
             About
           </a>
         </li>
         <li className='nav-item'>
           <a
             className='nav-link'
-            href='#'>
+            href='#services'>
             Services
           </a>
         </li>
         <li className='nav-item'>
           <a
             className='nav-link'
-            href='#'>
+            href='#/contact'>
             Contact
           </a>
         </li>
